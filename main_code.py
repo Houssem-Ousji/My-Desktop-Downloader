f46ac54ae1fc6116eb8fec9719c337331acb482f
unknown <ousjihoussem01@gmail.com>
2021-06-26 20:00:39 +0200

    Finishing the second section

--- main_code.py
+++ main_code.py
@@ -9,7 +9,8 @@ import sys
 from os import path
 
 import urllib.request
-
+import pafy
+import humanize
 ui,_ = loadUiType('Downloader.ui')
 
 class Mainapp(QMainWindow, ui):
@@ -41,7 +42,19 @@ class Mainapp(QMainWindow, ui):
         self.pushButton_3.clicked.connect(self.Handle_Download_section_1)
         self.pushButton_4.clicked.connect(self.Browse_section_1)
 
-    # Saving and Showing the path
+        # Buttons of the section 2
+        self.pushButton_2.clicked.connect(self.Handle_Download_Youtube_Video)
+        self.pushButton.clicked.connect(self.Browse_section_2)
+        self.pushButton_5.clicked.connect(self.Quality_section_2)
+
+        # Butttons of the section 3
+
+
+
+
+
+############################# Start first section ################################
+    # Saving and Showing the path of the first section
     def Browse_section_1(self):
         location = QFileDialog.getSaveFileName(self, caption="Save as", directory="C:/Users/HOUSSEM/Desktop", filter="All files ()")
         self.lineEdit_4.setText(location[0])
@@ -70,6 +83,64 @@ class Mainapp(QMainWindow, ui):
         self.lineEdit_3.setText("")
         self.lineEdit_4.setText("")
         self.progressBar_2.setValue(0)
+############################# END first section ################################
+
+############################# Start Second section ################################
+    # Saving and Showing the path of the seconb section
+    def Browse_section_2(self):
+        location = QFileDialog.getExistingDirectory(self, caption= "Choose Folder", directory="C:/Users/HOUSSEM/Desktop")
+        self.lineEdit_2.setText(location)
+
+    # Our video Quality and the Media type
+    def Quality_section_2(self):
+        url = self.lineEdit.text()
+        test = True
+        try:
+           video = pafy.new(url)
+        except:
+            QMessageBox.warning(self, "Error 404", "Url Doesn't exist !")
+            self.lineEdit.setText('')
+            test = False
+        if test == True:
+            for Stream in video.allstreams:
+               file_size = humanize.naturalsize(Stream.get_filesize())
+               video_data = "{} : {} : {} : {}".format(Stream.mediatype, Stream.extension, Stream.quality, file_size)
+               self.comboBox.addItem(video_data)
+
+    # Making the Progress bar
+    def Handle_Progress_Bar_Section_2(self, *Data_recieved):
+        percentage = (Data_recieved[1] / Data_recieved[0]) *100
+        self.progressBar.setValue(int(percentage))
+        QApplication.processEvents()
+    
+    # Download the Youtube Video
+    def Handle_Download_Youtube_Video(self):
+        url = self.lineEdit.text()
+        test = True
+        try:
+            video = pafy.new(url)
+        except:
+            QMessageBox.warning(self, "Error 404", "Url Doesn't exist !")
+            self.lineEdit.setText('')
+            test = False
+        if test == True:
+            path = self.lineEdit_2.text()
+            video_index = self.comboBox.currentIndex()
+            try:
+                Download = video.allstreams[video_index].download(filepath=path, callback=self.Handle_Progress_Bar_Section_2)
+                QMessageBox.information(self, "Download Done", "successful download")
+            except:
+                QMessageBox.warning(self, "Download error", "download failed")
+
+            # Reset Our Inputs
+            self.lineEdit.setText('')
+            self.lineEdit_2.setText('')
+            self.comboBox.clear()
+############################# END first section ################################
+
+
+
+
 
 
 
